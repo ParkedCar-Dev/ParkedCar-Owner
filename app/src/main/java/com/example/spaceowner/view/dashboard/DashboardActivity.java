02054eb96709ac0544e0155e3b7f4dee5fa06178
Mobaswirul Islam <1805090@ugrad.cse.buet.ac.bd>
2023-08-06 12:47:36 +0600

    dashboard to addspace through fabutton

--- app/src/main/java/com/example/spaceowner/view/dashboard/DashboardActivity.java
+++ app/src/main/java/com/example/spaceowner/view/dashboard/DashboardActivity.java
@@ -6,15 +6,19 @@ import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.viewpager2.widget.ViewPager2;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.example.spaceowner.R;
+import com.example.spaceowner.view.addspace.AddSpaceActivity;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.tabs.TabLayout;
 
 public class DashboardActivity extends AppCompatActivity {
     TabLayout tabLayout;
     ViewPager2 viewPager;
     DashboardViewpagerAdapter adapter;
+    FloatingActionButton addSpaceButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,6 +26,7 @@ public class DashboardActivity extends AppCompatActivity {
 
         tabLayout = findViewById(R.id.tab_layout_dashboard);
         viewPager = findViewById(R.id.dashboard_viewpager);
+        addSpaceButton = findViewById(R.id.fab_add_space);
         adapter = new DashboardViewpagerAdapter(this);
         viewPager.setAdapter(adapter);
 
@@ -40,6 +45,13 @@ public class DashboardActivity extends AppCompatActivity {
             public void onTabReselected(TabLayout.Tab tab){
 
             }
+        });
+
+        addSpaceButton.setOnClickListener((v) -> {
+//            go to add space activity
+            Intent intent = new Intent(this, AddSpaceActivity.class);
+            startActivity(intent);
+            this.finish();
         });
 
     }
